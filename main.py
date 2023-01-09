ef6eba00a6d4f382ecbee67c2136cd6a90f0f005
Joanna Oleszczuk <jooleszczuk@wp.pl>
2023-01-09 19:47:35 +0100

    Add lifes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import random as r
 MountainList = [["Sniezka", 1602], ["Babia Gora", 1725], ["Snieznik", 1423], ["Giewont", 1895], ["Rysy", 2499], ["Kasprowy Wierch", 1987], ["Koscielec", 2155]]
 points = 0
+totalLife = 3
+life = 3
 
 for i in MountainList:
     RandomValue = r.randrange(0, len(MountainList)-1)
-    AskUser = (input("(Current points:" + str(points) + ")"+ " What is the high of " + MountainList[RandomValue][0] + "?"))
+    AskUser = (input("(Current points:" + str(points) + " , life availability: " + str(life) + "/" + str(totalLife) +") " + " What is the high of " + MountainList[RandomValue][0] + "?"))
 
     if int(AskUser) == MountainList[RandomValue][1]:
         print("Good answer")
         points += 1
     else:
-        points -= 1
+        life -= 1
         print("Wrong answer")
+        if life == 0:
+            print("Game is over")
+            break
