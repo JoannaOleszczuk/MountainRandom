7a4b43cbb92c8898860d36a03abe7db011bf3bdc
Joanna Oleszczuk <jooleszczuk@wp.pl>
2023-01-15 15:19:17 +0100

    removing from db WIP

--- settings.py
+++ settings.py
@@ -1,11 +1,30 @@
+DB_FILE_NAME = "MountainsList2.txt"
 
+def LoadFile(filepath):
+    MountainListFromFile = []
+    f = open(filepath, "r")
+    MountainList = f.readlines()
+
+    for i in MountainList:
+        i = i[:-1]
+        MountainListFromFile.append(i.split(","))
+
+    counter = 0
+    for i in MountainListFromFile:
+        MountainListFromFile[counter][1] = int(i[1])
+        counter += 1
+
+    f.close()
+    return MountainListFromFile
+
+# print(LoadFile("MountainsList2.txt"))
 
 def settings():
-    AskUser = input("Choose if you would like to add position to database or just show it. Press 1 to add, press 2 to show")
+    AskUser = input("Choose if you would like to add, remove or show position in database. Press 1 to add, press 2 to remove, press 3 to show")
 
     if int(AskUser) == 1:
         while True:
-            filepath = "MountainsList.txt"
+            filepath = DB_FILE_NAME
             f = open(filepath, "a")
             mountain = input("Type name of the mountain: ")
             high = input("Type high of the mountain: ")
@@ -20,19 +39,21 @@ def settings():
                 continue
 
     elif int(AskUser) == 2:
-        MountainListFromFile = []
-        filepath = "MountainsList.txt"
-        f = open(filepath, "r")
-        MountainList = f.readlines()
-
-        for i in MountainList:
-            i = i[:-1]
-            MountainListFromFile.append(i.split(","))
+        AskForRemoving = input("Type the Mountain to remove it from game: ")
+        tmp = LoadFile(DB_FILE_NAME)
 
         counter = 0
-        for i in MountainListFromFile:
-            MountainListFromFile[counter][1] = int(i[1])
-            counter += 1
+        for i in tmp:
+            if AskForRemoving == i[0]:
+                tmp.pop(counter)
+            counter +=1
+        print("Updated list: " , tmp)
+        filepath = DB_FILE_NAME
+        f = open(filepath, "w")
 
-        print(MountainListFromFile)
+        f.write(tmp)
         f.close()
+
+
+    elif int(AskUser) == 3:
+        print(LoadFile(DB_FILE_NAME))
