b2a5cdab1def16f28cf3251dc42639838f985ac1
Joanna Oleszczuk <jooleszczuk@wp.pl>
2023-01-24 18:45:19 +0100

    add exception handling

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import random as r
+import sys
+
+
 # MountainList = [["Sniezka", 1602], ["Babia Gora", 1725], ["Snieznik", 1423], ["Giewont", 1895], ["Rysy", 2499], ["Kasprowy Wierch", 1987], ["Koscielec", 2155]]
 
 
@@ -27,14 +30,18 @@ def game():
 
     for i in MountainListFromFile:
         RandomValue = r.randrange(0, len(MountainListFromFile)-1)
-        AskUser = (input("(Current points:" + str(points) + " , life availability: " + str(life) + "/" + str(totalLife) +") " + " What is the high of " + MountainListFromFile[RandomValue][0] + "?"))
 
-        if int(AskUser) == MountainListFromFile[RandomValue][1]:
-            print("Good answer")
-            points += 1
-        else:
-            life -= 1
-            print("Wrong answer")
-            if life == 0:
-                print("Game is over")
-                break
+        try:
+            AskUser = (input("(Current points:" + str(points) + " , life availability: " + str(life) + "/" + str(totalLife) +") " + " What is the high of " + MountainListFromFile[RandomValue][0] + "?"))
+
+            if int(AskUser) == MountainListFromFile[RandomValue][1]:
+                print("Good answer")
+                points += 1
+            else:
+                life -= 1
+                print("Wrong answer")
+                if life == 0:
+                    print("Game is over")
+                    break
+        except:
+            print("You type wrong type of value. Occurs error: ", sys.exc_info())
